7460408336cd5349a976d699f9967f2031e5b54f
iamarirosenthal <iamarirosenthal@outlook.com>
2017-11-17 15:48:46 -0500

    new stuff

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendList = [
+  {
+    name: 'Ari Rosenthal',
+    profilePic: 'https://1drv.ms/i/s!Ahf65a4AI_yShDxR2w36B3k5OKr4',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
